f839b24cd97a518ace46ea1608d3eb50620dcb0c
Anthony Malgapo <anthony.malgapo@gmail.com>
2017-05-26 00:48:13 -0400

    adding daily workings of to do list to repo

--- todo_script.js
+++ todo_script.js
@@ -1,10 +1,15 @@
-$(document).ready(function(){
-    $('#button').click(function(){
+// jQuery starts here
+$(document).ready(function(){ // calling the document and making sure it's ready for jQuery
+    $('#button').click(function(){ // adding a click function to the button div
+        // creating a jQuery variable to take the user input for objective
         var toAdd = $('input[name=checkListItem]').val();
+        // creating a jQuery variable to take the user input for time
         var time = $('input[name=checkListTime]').val();
-        $('.list').append('<div class="item">' + toAdd + ' - @' + time + '</div>');
+        $('ol').append('<li>' + toAdd + ' - @' + time + '</li>'); // on click, add the objective and time to the list
     });
-    $('#printbutton').click(function(){
+    $('#howitworks').accordion({collapsible: true, active: false}); // accordion effect on the how it works 'button'
+    $('#printbutton').click(function(){ // on click of the print button, access the print function
       window.print();
     });
+    $('ol').selectable(); // expanding on this later. selecting the different objectives adds color and neatness
 });
