9ecc4a0267167cdf3c99ad6bd2211868cd59fb95
Anthony Malgapo <anthony.malgapo@gmail.com>
2017-05-28 02:51:29 -0400

    also pushing to the jQuery repo

--- todo_script.js
+++ todo_script.js
@@ -6,6 +6,9 @@ $(document).ready(function(){ // calling the document and making sure it's ready
         // creating a jQuery variable to take the user input for time
         var time = $('input[name=checkListTime]').val();
         $('ol').append('<li>' + toAdd + ' - @' + time + '</li>'); // on click, add the objective and time to the list
+        // on click, reset the textboxes so user has easier time adding more things to do
+        $('input[name=checkListItem]').val('');
+        $('input[name=checkListTime]').val('');
     });
     $('#howitworks').accordion({collapsible: true, active: false}); // accordion effect on the how it works 'button'
     $('#printbutton').click(function(){ // on click of the print button, access the print function
